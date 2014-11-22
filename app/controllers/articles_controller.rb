71f6b1378f1fdada231c9f593aa8ef0d501925ee
manojdevs <manojselvamdev@gmail.com>
2014-11-22 11:18:00 +0530

    athuentic update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,51 +1,47 @@
 class ArticlesController < ApplicationController
-	
+	before_filter :require_login, only: [:new,:index,:show, :create, :edit, :update, :destroy]
+	include ArticlesHelper
 	def index
-		if signed_in?
 	@article=Article.all
-end
 	end
 	def show
-		if signed_in?
+		
 	@article=Article.find(params[:id])	
 	@comment = Comment.new
     @comment.article_id = @article.id 	
-end
 	end
 	def new
-		if signed_in?
 	@article=Article.new
-end
 	end
 	def create
-		if signed_in?
+		
 	@article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
-end
+
 	end
 	def destroy
-		if signed_in?
+		
 	article=Article.find(params[:id])
     article.destroy
     redirect_to articles_path
-end
+
 	end
 	def edit
-		if signed_in?
 	 @article = Article.find(params[:id])	
 	end
-	end
 	def update
-		if signed_in?
 	@article = Article.find(params[:id])
     @article.update(article_params)
     flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to article_path(@article)
-end
 	end
 	def article_params
     params.require(:article).permit(:title, :body,:image)
     end
-    
+    def require_login
+    	unless signed_in?
+    		redirect_to root_path
+    	end
+    end
 end
